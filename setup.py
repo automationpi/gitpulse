d193cceb7ccc883a5608651e9936a455565d5242
automationpi <alok.naveen@gmail.com>
2023-04-15 11:54:43 +0200

    setup.py ready.

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ setup(
     version='1.0',
     packages=find_packages(),
     install_requires=[
-        # Add your package dependencies here, for example:
-        # 'jinja2',
+        # Add your package dependencies here:
+        'gitpython',
+        'jinja2',
+        'python-dateutil',
     ],
     entry_points={
         'console_scripts': [
